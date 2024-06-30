bb8c189ef16605058db79d561296710c81a32a02
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-06-30 16:04:24 +0400

    Implement Softmax from scratch using Numpy

---
+++ activation/SoftMax.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class SoftMax:
+
+
+    def forward(self, inputs):
+        """ Softmax first exponentiates the inputs the power of e(euler number)
+        Then it sum ups all exponentiated values and divides each input in order to get probability distribution
+
+        axis=1 specifies column-wise use of list
+
+        keepdims=True makes the shape of the output same as input shape
+
+        We substract the maximum number from inputs in order to avoid exponential function overflow, or dead neurons
+        """
+
+        exp_values=np.exp(inputs - np.max(inputs, axis=1,
+                                                keepdims=True))
+        probabilities= exp_values/np.sum(exp_values, axis=1,
+                                                     keepdims=True)
+        self.output=probabilities
+
+
