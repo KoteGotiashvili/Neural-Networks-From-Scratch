ebb8a70be928588da136131143b0d982201228b6
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-06-30 16:28:03 +0400

    Update main.py add second dense layer and pass ReLU activation output to second dense and then perform forward pass through softmax

--- runningmodels/main.py
+++ runningmodels/main.py
@@ -5,11 +5,17 @@ from activation.ReLU import RelU
 from activation.SoftMax import SoftMax
 X, y = spiral_data(samples=100,
                    classes=3)
-# Create Dense Layer with 2 input and 3 output
+# Create Dense Layer for ReLU
 dense1 = Dense(2,3)
+
+#Create dense 2 for softmax, (3,3) because we take output from previous layer (2,3) -> (2,3) @ (3,3)
+dense2 = Dense(3,3)
 
 # Initialize Relu to be used with dense
 activation1=RelU()
+
+#Initialize SoftMa
+activation2=SoftMax()
 
 # make forward pass through training spiral data
 dense1.forward(X)
@@ -17,8 +23,15 @@ dense1.forward(X)
 # Forward pass through activation function
 activation1.forward(dense1.output)
 
+#Make forward through second dense layer
+dense2.forward(activation1.output)
+
+#make forward pass through activation function(softmax)
+activation2.forward(dense2.output)
+
+# pirnt ReLU results
 print(activation1.output[:5])
 
-softmax=SoftMax()
-softmax.forward([[1,2,3]])
-print(softmax.output)
+# Print Softmax Results, After That developer in many cases apply ARGMAX(to get most probable output, just choose max prob val)
+print(activation2.output[:5])
+
