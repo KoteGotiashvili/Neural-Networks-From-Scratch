aa4a265a4efe2bd7a0b905c5e535408d856eeab2
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-05 13:04:25 +0400

    Add backward method in Dense and Relu class

--- activation/ReLU.py
+++ activation/ReLU.py
@@ -4,3 +4,11 @@ class RelU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
+    def backward(self, dvalues):
+
+        # create copy since we need to modify original one
+        self.dinputs=dvalues.copy()
+
+        # Zero gradient where input values were negative
+        self.dinputs[self.dinputs<=0] = 0
+
--- models/Dense.py
+++ models/Dense.py
@@ -10,3 +10,27 @@ class Dense:
 
     def forward(self, input):
         self.output = np.dot(input, self.weights) + self.biases
+
+    def backward(self, dvalues):
+
+        """
+         Calculate the gradient of the cost with respect to weights and biases.
+         dvalues: gradient of the cost with respect to the output of the layer.
+         input: input of the layer.
+
+         Returns:
+         dweights: gradient of the cost with respect to the weights
+         dbiases: gradient of the cost with respect to the biases
+
+         Note: The dinputs of the previous layer is not calculated in this method.
+         It will be calculated in the method `update_weights` in the `NeuralNetwork` class.
+
+
+        """
+        # Calculate gradient of weights and biases
+
+        self.dweights = np.dot(self.input.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+
+        # Calculate gradient of the input
+        self.dinputs = np.dot(dvalues, self.weights.T)
