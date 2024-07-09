55c401208e52051f4aa9e7208917f47b23581709
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-09 14:08:40 +0400

    modify SGD class based on learning rate decay logic, update params based on lr decay formula

--- optimizers/SGD.py
+++ optimizers/SGD.py
@@ -1,12 +1,30 @@
 
 class SGD:
-    def __init__(self, lr=1):
+    def __init__(self, lr=1, decay=0.):
         """
-        Initialize the Stochastic Gradient Descent optimizer.
         Args:
-        lr (float): The learning rate for the optimizer. Default is 1.
+        lr (float): The initial learning rate.
+        decay (float): The decay rate for the learning rate.
+        current_lr(float): updated current learning rate
+        iterations: epochs/steps
         """
         self.lr = lr
+        self.current_lr=lr
+        self.decay = decay
+        self.iterations = 0
+
+
+    def pre_update_params(self):
+        """
+        Update the learning rate at the beginning of each iteration.
+        The learning rate is decayed by a factor of 1/(1+decay*iterations)
+        to prevent the learning rate from becoming too small, or large.
+
+        Returns:
+        None
+        """
+        if self.decay:
+            self.current_lr = self.lr * (1./(1. + self.decay * self.iterations))
 
     def update_params(self, layer):
         """
@@ -18,5 +36,14 @@ class SGD:
         None
         """
         # multiply learning rate by derivatives of weights and biases
-        layer.weights+= -self.lr * layer.dweights
-        layer.biases+= -self.lr * layer.dbiases
+        layer.weights += -self.current_lr * layer.dweights
+        layer.biases += -self.current_lr * layer.dbiases
+
+    def post_update_params(self):
+        """
+        Reset the gradients of the layer for the next iteration.
+
+        Returns:
+        None
+        """
+        self.iterations += 1
