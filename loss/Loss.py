fcebb1cdf6f475d31e0bd686ca315aa9969afdc3
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-13 18:33:23 +0400

    create regularization loss method and calculate L1 and L2 regularization loss and sum up

--- loss/Loss.py
+++ loss/Loss.py
@@ -10,3 +10,26 @@ class Loss:
         data_loss=np.mean(sample_losses)
 
         return data_loss
+    def regularzation_loss(self, layer):
+
+        """
+        Calculate regularization loss both L1 and L2
+
+        :return: float
+        """
+
+        regularization_loss = 0
+
+        if layer.weight_regularization_L1 > 0:
+            regularization_loss += layer.weight_regularization_L1 * np.sum(np.abs(layer.weights))
+
+        if layer.weight_regularization_L2 > 0:
+            regularization_loss += layer.weight_regularization_L2 * np.sum(layer.weights ** 2)
+
+        if layer.bias_regularization_L1 > 0:
+            regularization_loss += layer.bias_regularization_L1 * np.sum(np.abs(layer.biases))
+
+        if layer.bias_regularization_L2 > 0:
+            regularization_loss += layer.bias_regularization_L2 * np.sum(layer.biases ** 2)
+
+        return regularization_loss
