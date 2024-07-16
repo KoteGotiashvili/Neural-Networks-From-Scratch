fae56ae0797633fe6d1c00e7d42e7b3b49d024ce
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-16 09:02:37 +0400

    Implement binary cross entropy from scratch with forward/backward pass

---
+++ loss/BinaryCrossEntropy.py
@@ -0,0 +1,34 @@
+import Loss
+import numpy as np
+
+class BinaryCrossEntropy(Loss):
+
+    def forward(self, y_pred, y_true):
+
+        #clip data to prevent division by zero
+        # Clip both sides in order to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # calculate binary cross entropy loss
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * np.log(1 - y_pred_clipped))
+
+        sample_losses = np.mean(sample_losses, axis=-1)
+
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+
+        #num of samples
+        samples = len(dvalues)
+
+        #num of labels in every sample
+        outputs = len(dvalues[0])
+
+        # clip to prevent divison by zero
+        dvalues_clipped = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # calculate gradientdvalues_clipped
+        self.dinputs = - (y_true / dvalues_clipped - (1-y_true)/(1 - dvalues_clipped)) / outputs
+
+        # normalize gradient
+        self.dinputs = self.dinputs / samples
