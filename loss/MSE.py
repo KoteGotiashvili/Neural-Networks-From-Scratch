a67d8b0b59ceb9d6f42727e48ba04670f6c5e9e0
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-17 10:14:33 +0400

    Implement MSE from scratch

---
+++ loss/MSE.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+#Mean Squared Error/Loss
+class MSE:
+
+    def forward(self, y_pred, y_true):
+        sample_losses = np.mean((y_pred - y_true)**2, axis=-1)
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+
+        # number of samples
+        samples = len(dvalues)
+        # number of output in every sample, use first sample to count
+        outputs = len(dvalues[0])
+
+        # gradient on values
+        self.dinputs = -2 * (y_true - dvalues) / samples
+
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
