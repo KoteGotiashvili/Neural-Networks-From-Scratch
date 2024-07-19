a5991971636a02c871ae4ff3d47d25917fda753a
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-19 14:57:39 +0400

    Test Model class, create 3 dense, 2 relu layer + last output linear layer, set loss, optimizer and train method without forward and backward pass, it will be implemented in near future

---
+++ model/TestModel.py
@@ -0,0 +1,37 @@
+from model.Model import Model
+from models.Dense import Dense
+from activation.ReLU import ReLU
+from activation.Linear import Linear
+from loss.MSE import MSE
+from optimizers.Adam import Adam
+from nnfs.datasets import sine_data
+
+# create dataset
+X, y = sine_data()
+
+model = Model()
+
+
+model.add(Dense(1, 64))
+model.add(ReLU())
+model.add(Dense(64,64))
+model.add(ReLU())
+model.add(Dense(64,1))
+model.add(Linear())
+
+# layers
+print(model.layers)
+
+
+model.set(
+    loss=MSE(),
+    optimizer=Adam(learning_rate=0.05, decay=1e-3)
+)
+
+
+model.train(X, y, epochs=10000, print_every=100)
+
+
+print(model.layers)
+
+
