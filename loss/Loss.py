4d40154cbc7d3db49ffb735faa4042eeae6c973d
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-20 18:59:08 +0400

    save trainable layers in Loss class with method

--- loss/Loss.py
+++ loss/Loss.py
@@ -3,33 +3,40 @@ import numpy as np
 
 class Loss:
 
+
+
+    def remember_trainable_layers(self, trainable_layers):
+        self.trainable_layers = trainable_layers
+
     # Regularization loss calculation
     def regularization_loss(self, layer):
 
         # 0 by default
         regularization_loss = 0
+        # calculate regularization loss, itarate only on trainable layers
+        for layer in self.trainable_layers:
 
-        # L1 regularization - weights
-        # calculate only when factor greater than 0
-        if layer.weight_regularizer_l1 > 0:
-            regularization_loss += layer.weight_regularizer_l1 * \
-                                   np.sum(np.abs(layer.weights))
+            # L1 regularization - weights
+            # calculate only when factor greater than 0
+            if layer.weight_regularizer_l1 > 0:
+                regularization_loss += layer.weight_regularizer_l1 * \
+                                       np.sum(np.abs(layer.weights))
 
-        # L2 regularization - weights
-        if layer.weight_regularizer_l2 > 0:
-            regularization_loss += layer.weight_regularizer_l2 * \
+            # L2 regularization - weights
+            if layer.weight_regularizer_l2 > 0:
+                regularization_loss += layer.weight_regularizer_l2 * \
                                    np.sum(layer.weights *
                                           layer.weights)
 
-        # L1 regularization - biases
-        # calculate only when factor greater than 0
-        if layer.bias_regularizer_l1 > 0:
-            regularization_loss += layer.bias_regularizer_l1 * \
+            # L1 regularization - biases
+            # calculate only when factor greater than 0
+            if layer.bias_regularizer_l1 > 0:
+                regularization_loss += layer.bias_regularizer_l1 * \
                                    np.sum(np.abs(layer.biases))
 
-        # L2 regularization - biases
-        if layer.bias_regularizer_l2 > 0:
-            regularization_loss += layer.bias_regularizer_l2 * \
+            # L2 regularization - biases
+            if layer.bias_regularizer_l2 > 0:
+                regularization_loss += layer.bias_regularizer_l2 * \
                                    np.sum(layer.biases *
                                           layer.biases)
 
