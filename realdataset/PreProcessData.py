4dac547408e694513eb36f959bc051ac5b73023b
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-25 20:25:45 +0400

    scale features betwwen -1 and 1 and reshape it to be vector

---
+++ realdataset/PreProcessData.py
@@ -0,0 +1,20 @@
+import numpy as np
+from realdataset.DataLoading import DataLoading
+
+class PreProcessData:
+
+    def scale_features(self, X, y, X_test, y_test):
+
+        """
+        Scale the features of the MNIST dataset between -1 and 1.
+
+        :return: Scaled image
+        """
+        X = (X.astype(np.float32) - 127.5) / 127.5
+        X_test = (X_test.astype(np.float32) - 127.5) / 127.5
+
+        # reshape to vectors, currently it is 2d
+        X = X.reshape(X.shape[0], -1)
+        X_test = X_test.reshape(X_test.shape[0], -1)
+
+        return X, y, X_test, y_test
