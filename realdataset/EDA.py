2791a6ba11774f70a18d3e3dba9504c34af3c7c7
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-25 20:14:28 +0400

    load dataset

--- realdataset/EDA.py
+++ realdataset/EDA.py
@@ -3,9 +3,14 @@ from realdataset.DataLoading import DataLoading
 DIR = "./fashion_mnist_images/train"
 GET_SOME_IMAGE_FROM_DIR = "./fashion_mnist_images/train/7/0017.png"
 
+# initialize dataloading
+dl = DataLoading()
+
 # get image labels, and find how many classes are there
 print("image labels, class count")
-DataLoading.get_labels(DIR)
+#DataLoading.get_labels(DIR)
 
 print("visualize data")
-DataLoading.visualize_data(GET_SOME_IMAGE_FROM_DIR)
+#DataLoading.visualize_data(GET_SOME_IMAGE_FROM_DIR)
+
+X, y, X_test, y_test = dl.create_data_mnist('./fashion_mnist_images')
