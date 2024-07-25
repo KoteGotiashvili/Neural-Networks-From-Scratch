669eb38d89e4a1e756a8c53a0bc708f3007c22f2
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-25 19:47:46 +0400

    get labels and visualize some image from dataset

---
+++ realdataset/EDA.py
@@ -0,0 +1,11 @@
+from realdataset.DataLoading import DataLoading
+
+DIR = "./fashion_mnist_images/train"
+GET_SOME_IMAGE_FROM_DIR = "./fashion_mnist_images/train/7/0017.png"
+
+# get image labels, and find how many classes are there
+print("image labels, class count")
+DataLoading.get_labels(DIR)
+
+print("visualize data")
+DataLoading.visualize_data(GET_SOME_IMAGE_FROM_DIR)
