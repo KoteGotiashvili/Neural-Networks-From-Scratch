12181c2398d91d4d057ec8ce749870444a8e6175
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-25 20:26:12 +0400

    make code that scales and turns img numbers into vectors

--- realdataset/EDA.py
+++ realdataset/EDA.py
@@ -1,5 +1,5 @@
 from realdataset.DataLoading import DataLoading
-
+from realdataset.PreProcessData import PreProcessData
 DIR = "./fashion_mnist_images/train"
 GET_SOME_IMAGE_FROM_DIR = "./fashion_mnist_images/train/7/0017.png"
 
@@ -14,3 +14,10 @@ print("visualize data")
 #DataLoading.visualize_data(GET_SOME_IMAGE_FROM_DIR)
 
 X, y, X_test, y_test = dl.create_data_mnist('./fashion_mnist_images')
+
+# preprocess data
+preprocess_data = PreProcessData()
+
+# scale data between -1 and 1, this is very tough approach but works fine for this case
+X, y, X_test, y_test = preprocess_data.scale_features(X, y, X_test, y_test)
+print(X.min(), X.max())
