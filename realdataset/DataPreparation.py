32e99aafcd019e6715da21d8d5c0d8011d50195e
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-25 17:53:04 +0400

    modifty code, create class DataPreparation

--- realdataset/DataPreparation.py
+++ realdataset/DataPreparation.py
@@ -2,26 +2,42 @@ import os
 import urllib
 import urllib.request
 from zipfile import ZipFile
-UTL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
-FILE = 'fashion_mnist_images.zip'
-FOLDER = 'fashion_mnist_images'
+class DataPreparation:
+    URL = 'https://nnfs.io/datasets/fashion_mnist_images.zip'
+    FILE = 'fashion_mnist_images.zip'
+    FOLDER = 'fashion_mnist_images'
 
-# Download dataset
-def download_dataset():
-    """
-    Download the Fashion-MNIST dataset from the given URL.
+    @staticmethod
+    def download_dataset():
+        """
+        Download the Fashion-MNIST dataset from the given URL.
 
-    :return: None
-    """
-    if not os.path.exists(FILE):
-        print(f'Downloading {FILE}...')
-        urllib.request.urlretrieve(UTL, FILE)
-def unzip_dataset():
-    print("unzipping images...")
-    with ZipFile(FILE, 'r') as zip_images:
-        zip_images.extractall(FOLDER)
+        :return: None
+        """
+        if not os.path.exists(DataPreparation.FILE):
+            print(f'Downloading {DataPreparation.FILE}...')
+            urllib.request.urlretrieve(DataPreparation.URL, DataPreparation.FILE)
+        else:
+            print(f'{DataPreparation.FILE} already exists.')
 
-#download_dataset()
-#unzip_dataset()
-#print("done")
+    @staticmethod
+    def unzip_dataset():
+        """
+        Unzip the downloaded dataset.
+
+        :return: None
+        """
+        if not os.path.exists(DataPreparation.FOLDER):
+            print("Unzipping images...")
+            with ZipFile(DataPreparation.FILE, 'r') as zip_images:
+                zip_images.extractall(DataPreparation.FOLDER)
+            print("Unzipping done.")
+        else:
+            print(f'{DataPreparation.FOLDER} already exists.')
+
+
+
+DataPreparation.download_dataset()
+DataPreparation.unzip_dataset()
+print("Done")
 
