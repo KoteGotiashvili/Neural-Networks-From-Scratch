65ae40e6a331a5423883470c809977a84bb32f14
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-26 12:50:18 +0400

    test data shuffling, visualize and compare labels to image

--- realdataset/EDA.py
+++ realdataset/EDA.py
@@ -1,5 +1,7 @@
 from realdataset.DataLoading import DataLoading
 from realdataset.PreProcessData import PreProcessData
+from realdataset.DataShuffling import DataShuffling
+import matplotlib.pyplot as plt
 DIR = "./fashion_mnist_images/train"
 GET_SOME_IMAGE_FROM_DIR = "./fashion_mnist_images/train/7/0017.png"
 
@@ -20,4 +22,13 @@ preprocess_data = PreProcessData()
 
 # scale data between -1 and 1, this is very tough approach but works fine for this case
 X, y, X_test, y_test = preprocess_data.scale_features(X, y, X_test, y_test)
-print(X.min(), X.max())
+# print("get min&max")
+#print(X.min(), X.max())
+
+# test suffle data
+X, y = DataShuffling.shuffle_data(X, y)
+#get labels
+print(y[:10])
+# now test if it right
+plt.imshow(X[1].reshape(28,28))
+plt.show()
