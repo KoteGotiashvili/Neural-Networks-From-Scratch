64d8dcd38b6dc8da029281fd4f8a76e428893d07
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-07-27 19:59:28 +0400

    add calculate accumalted method which do same thing as in Loss class

--- accuracy/Accuracy.py
+++ accuracy/Accuracy.py
@@ -11,4 +11,21 @@ class Accuracy:
         # calculate an accuracy
         accuracy = np.mean(comparisons)
 
+        # add accumalted sum of matching values and sample count
+        self.accumulated_sum += np.sum(comparisons)
+        self.accumulated_count += len(comparisons)
+
         return accuracy
+
+    def calculate_accumulated(self):
+        """"
+        Calculates the accumulated accuracy.
+
+        :return: Accumulated accuracy.
+        """
+
+        #calculate mean accuracy
+        accuracy = self.accumulated_sum / self.accumulated_count
+
+        return accuracy
+
