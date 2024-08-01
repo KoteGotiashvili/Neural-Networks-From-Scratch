cd1d74c9677c1ac0a96dc7bf776ca81f3afd3879
KoteGotiashvili <kote.gotiashvili@gmail.com>
2024-08-01 17:07:20 +0400

    test on data from google

--- realdataset/TestOnRealDataset.py
+++ realdataset/TestOnRealDataset.py
@@ -61,11 +61,42 @@ X, X_test = data_preprocess.scale_features(X, X_test)
 # confidences = model.predict(X_test[:5])
 # print(confidences)
 
+# Label index to label name relation
+fashion_mnist_labels = {
+    0: 'T-shirt/top',
+    1: 'Trouser',
+    2: 'Pullover',
+    3: 'Dress',
+    4: 'Coat',
+    5: 'Sandal',
+    6: 'Shirt',
+    7: 'Sneaker',
+    8: 'Bag',
+    9: 'Ankle boot'
+}
 
-# lets load image from google
-image_data = cv2.imread('tshirt.jpg', cv2.IMREAD_GRAYSCALE)
-image_data = cv2.resize(image_data, (28,28))
-plt.imshow(image_data, cmap='gray')
-plt.show()
+# Read an image
+image_data = cv2.imread('coat.jpg', cv2.IMREAD_GRAYSCALE)
 
+# Resize to the same size as Fashion MNIST images
+image_data = cv2.resize(image_data, (28, 28))
 
+# Invert image colors
+image_data = 255 - image_data
+
+# Reshape and scale pixel data
+image_data = (image_data.reshape(1, -1).astype(np.float32) - 127.5) / 127.5
+
+# Load the model
+model = Model.load('fashion_mnist.model')
+
+# Predict on the image
+confidences = model.predict(image_data)
+
+# Get prediction instead of confidence levels
+predictions = model.output_layer_activation.predictions(confidences)
+
+# Get label name from label index
+prediction = fashion_mnist_labels[predictions[0]]
+
+print(prediction)
